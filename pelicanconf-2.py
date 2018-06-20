49dade516e0080a9fe8f7f0faeac7adf25864d11
ghxdghxd <guojt-4451@163.com>
2018-06-20 17:05:26 +0800

    修改

--- pelicanconf-2.py
+++ pelicanconf-2.py
@@ -4,22 +4,25 @@ from __future__ import unicode_literals
 
 AUTHOR = 'JT Guo'
 SITENAME = 'DGuo Blog'
-SITEURL = 'http://ghxdghxd.github.io'
-GITHUB_URL = 'http://github.com/ghxdghxd'
+SITESUBTITLE = '好记性不如烂键盘'
+SITEURL = 'https://ghxdghxd.github.io'
+GITHUB_URL = 'https://github.com/ghxdghxd'
 PATH = 'content'
 
 TIMEZONE = 'Asia/Shanghai'
 DEFAULT_LANG = 'zh'
 
-STATIC_PATHS = ["pages",'theme/images', "documents", 'images']
+STATIC_PATHS = ["pages", 'theme/images', "documents", 'images']
 EXTRA_PATH_METADATA = {
-    'pages/about.md': {'path': 'about.html'} # about.md生成到output下, 表示about.html的路径
+    # about.md生成到output下, 表示about.html的路径
+    'pages/about.md': {'path': 'about.html'}
 }
 PLUGIN_PATHS = ['plugins']
 PLUGINS = ['tipue_search', "sitemap", "i18n_subsites", "neighbors"]
 
-THEME = "themes/maupassant-pelican"
-DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'archives', 'search']
+THEME = "themes/webNote"
+DIRECT_TEMPLATES = ['index', 'tags', 'categories',
+                    'archives', 'search', 'minsearch']
 # title, link, font-awesome-id(http://fontawesome.io)
 MENUITEMS = [('Home', '.', 'fa-home'),
              ('Archive', 'archives.html', 'fa-archive'),
@@ -43,7 +46,7 @@ SITEMAP = {
     }
 }
 
-DISPLAY_CATEGORIES_ON_MENU = True # 显示分类
+DISPLAY_CATEGORIES_ON_MENU = True  # 显示分类
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -63,8 +66,7 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 PAGINATED_DIRECT_TEMPLATES = ['index']  # 分页
-DEFAULT_PAGINATION = 3
+DEFAULT_PAGINATION = 6
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
-
